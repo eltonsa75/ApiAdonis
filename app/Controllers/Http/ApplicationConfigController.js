fb765580b16b60ca9343520f678a31323d45d44a
Elton <eltonsa75@hotmail.com>
2021-02-07 19:14:28 -0300

    Ajuste final do Tempo Decorrido

--- app/Controllers/Http/ApplicationConfigController.js
+++ app/Controllers/Http/ApplicationConfigController.js
@@ -39,68 +39,61 @@ class ApplicationConfigController {
 
    async store ({ params: { id }, request, auth, response}) {
 
-    //#APIALTERADA#
+    // Retorno da API
+    let app2
+    app2 = await ApplicationConfig.query()
+     .select('customers.fantasy_name', 
+     'customer_offices.customer_office_name', 
+     'business_units.business_unit_name', 
+     'areas.area_name', 
+     'questionnaire_forms.questionnaire_form_name',
+     'application_configs.questionnaire_version_id',
+     'question.id as question_id'
+     )
+     .leftJoin('customers', 'application_configs.customer_id', 'customers.id')
+     .leftJoin('customer_offices', 'application_configs.customer_office_id', 'customer_offices.id')
+     .leftJoin('business_units', 'application_configs.business_unit_id', 'business_units.id')
+     .leftJoin('areas', 'application_configs.area_id', 'areas.id')
+     .leftJoin('questionnaire_versions', 'application_configs.questionnaire_version_id', 'questionnaire_versions.id' )
+     .leftJoin('questionnaire_forms', 'questionnaire_versions.questionnaire_form_id', 'questionnaire_forms.id' )
+     .leftJoin('question', 'question.questionnaire_version_id', 'questionnaire_versions.id' )  
+     .where('application_configs.id', '=', request.params.id)
+     .orderBy('question.question_edited_number')
+     .limit(1)  
+     .fetch()
   
-     try {
-            let app = await ApplicationConfig.findOrFail(request.params.id)
-            app.merge(request.all())
-            await app.save()
-         } catch (error) {
-            return response.status(400).send({
-              message: "Erro ao não encontrar a configuração da aplicação!"
-            })
-         }
-  
-     // Retorno da API
-     let app2
-     app2 = await ApplicationConfig.query()
-      .select('customers.fantasy_name', 
-      'customer_offices.customer_office_name', 
-      'business_units.business_unit_name', 
-      'areas.area_name', 
-      'questionnaire_forms.questionnaire_form_name',
-      'application_configs.questionnaire_version_id',
-      'question.id as question_id'
-      )
-      .leftJoin('customers', 'application_configs.customer_id', 'customers.id')
-      .leftJoin('customer_offices', 'application_configs.customer_office_id', 'customer_offices.id')
-      .leftJoin('business_units', 'application_configs.business_unit_id', 'business_units.id')
-      .leftJoin('areas', 'application_configs.area_id', 'areas.id')
-      .leftJoin('questionnaire_versions', 'application_configs.questionnaire_version_id', 'questionnaire_versions.id' )
-      .leftJoin('questionnaire_forms', 'questionnaire_versions.questionnaire_form_id', 'questionnaire_forms.id' )
-      .leftJoin('question', 'question.questionnaire_version_id', 'questionnaire_versions.id' )  
-      .where('application_configs.id', '=', request.params.id)
-      .orderBy('question.question_edited_number')
-      .limit(1)  
-      .fetch()
-   
-     //Obtém o Id do usuário
-     //const usuarioLogado = await auth.getUser()
-     //const user_id = usuarioLogado.id;
-     const user_id = 25
-     
-     /* Atualiza o registro de user_parameters */
-     const affectedRows = await Database  
-     .table('user_parameters')
-     .where('id', user_id)
-     .update({ application_config_id: request.params.id, 
-               questionnaire_version_id: app2.rows[0].questionnaire_version_id })
-  
-     /* Inicia a primeira questão do formulário - somente quando inicia o questionário  */
-     try {
-      const affectedRows2 = await Database  
-      .table('application_configs')
-      .where('id', request.params.id)
-      .andWhere('question_to_present', 0)
-      .update({ question_to_present: app2.rows[0].question_id }) 
-      } catch (error) {
-  
-      }   
-     
-     //console.log('Retorno da API',  app2.rows[0])
-     return response.status(201).send(app2.rows[0])
-  
-   }
+    //Obtém o Id do usuário
+    //const usuarioLogado = await auth.getUser()
+    //const user_id = usuarioLogado.id;
+    const user_id = 25
+    
+    /* Atualiza o registro de user_parameters */
+    const affectedRows = await Database  
+    .table('user_parameters')
+    .where('id', user_id)
+    .update({ application_config_id: request.params.id, 
+              questionnaire_version_id: app2.rows[0].questionnaire_version_id })
+ 
+    /* Inicia a primeira questão do formulário - somente quando inicia o questionário  */
+    /***UPDATE QUESTION_TO_PRESENT***/
+    
+    try {
+     const affectedRows2 = await Database  
+     .table('application_configs')
+     .where('id', request.params.id)
+     .andWhere('question_to_present', 0)
+     .update({ question_to_present: app2.rows[0].question_id }) 
+     } catch (error) {
+ 
+     }   
+    
+     console.log('register_ question_to_present: '+app2.rows[0].question_id)
+     console.log('register_ application_config_id: '+request.params.id)
+ 
+    //console.log('Retorno da API',  app2.rows[0])
+    return response.status(201).send(app2.rows[0])
+ 
+  }
 
   // Retorno do formulário Entrevista
   async show ({ params: { id }, request, response}) {
--- app/Controllers/Http/QuestionController.js
+++ app/Controllers/Http/QuestionController.js
@@ -4,6 +4,7 @@ const Questions = use("App/Models/Question")
 const Resposta = use("App/Models/QuestionResp")
 const QuestionResp = use("App/Models/QuestionResp")
 const QuestionRespSeq = use("App/Models/QuestionRespSeq")
+const UserParameter = use ("App/Models/UserParameter")
 const Database = use('Database')
 
 
@@ -130,16 +131,27 @@ async primeiraQuestao ({ request, auth, response}) {
   //const user_id = usuarioLogado.id;
   //#APIALTERADA#
 
-  const user_id = 25
+ const user_id = 25
+
+ // Leitura dos parâmetros do usuário logado para obter primeira questão
+ let user_param
+ user_param = await UserParameter.query()
+  .select(
+  'user_parameters.application_config_id',
+  'user_parameters.questionnaire_version_id'
+  )
+  .where('user_parameters.id', '=', user_id)
+  .fetch()
 
   return await Questions.query()
-  .innerJoin('questionnaire_versions', 'question.questionnaire_version_id', 'questionnaire_versions.id' )
-  .innerJoin('questionnaire_forms', 'questionnaire_versions.questionnaire_form_id', 'questionnaire_forms.id' )
-  .innerJoin('application_configs', 'question.id', 'application_configs.question_to_present' )
+  .innerJoin('application_configs', 'application_configs.question_to_present', 'question.id' )
+  .innerJoin('questionnaire_versions', 'questionnaire_versions.id', 'question.questionnaire_version_id' )
   .innerJoin('user_parameters', 'user_parameters.application_config_id', 'application_configs.id' )
-  .innerJoin('question_theme', 'question.question_theme_id', 'question_theme.id' )    
-  .leftOuterJoin('question_resps', 'question.id', 'question_resps.question_id')
+  .innerJoin('question_theme', 'question_theme.id', 'question.question_theme_id' )    
+  .leftOuterJoin('question_resps', 'question_resps.question_id', 'question.id' )
   .where('user_parameters.id', user_id)
+  .andWhere('questionnaire_versions.id', user_param.rows[0].questionnaire_version_id)
+  .andWhere('application_configs.id', user_param.rows[0].application_config_id)
   .select(
     'question.id',
     'application_configs.id as application_config_id',
@@ -162,16 +174,12 @@ async primeiraQuestao ({ request, auth, response}) {
     'application_configs.current_session_start_time',
     'application_configs.current_session_end_time',
     'application_configs.current_session_elapsed_time',
-    'application_configs.interview_total_elapsed_time',
-    'application_configs.status'
+    'application_configs.interview_total_elapsed_time'
     )
-
   .orderBy('question_edited_number')
   .limit(1)
   .fetch();
 }
-
-
 
 async proxima ({request}) {
   return await Questions.query()
@@ -281,181 +289,232 @@ async proxima ({request}) {
 
 
 // Atualizando o Método save_and_next
-async save_and_next ({request}) {
-  const body = JSON.parse(request.body.json);
-  console.log(body);
-  const now = new Date();
-  const user_id = 25 //Id fixo enquanto não houver como obter usuário logado
-  
-  /* Verifica se a questão já foi respondida para esta aplicação */
-  var question_resp_id = 0;
-  try {
-        /* Obtém o id da resposta se existir, senão inicia com 0 */    
-        const query_resp = await Database.select('id')
-            .from('question_resps')
-            .where(function () {
-              this
-              .where('application_config_id', body.answer.application_config_id)
-              .andWhere('question_id', body.answer.question_id)
-            })
+ /*************** */
+  /* Save_And_Next */
+  /*************** */
+  async save_and_next ({request}) {
+    
+    const body = JSON.parse(request.body.json);
+    console.log(body);
+    const now = new Date();
+    const user_id = 25 //Id fixo enquanto não houver como obter usuário logado
+    
+    /* Verifica se a questão já foi respondida para esta aplicação */
+    var question_resp_id = 0;
+    try {
+          /* Obtém o id da resposta se existir, senão inicia com 0 */    
+          const query_resp = await Database.select('id')
+              .from('question_resps')
+              .where(function () {
+                this
+                .where('application_config_id', body.answer.application_config_id)
+                .andWhere('question_id', body.answer.question_id)
+              })
 
-        try {
-           question_resp_id = query_resp[ 0 ].id
-        }  catch(error) {
-           question_resp_id = 0
+          try {
+             question_resp_id = query_resp[ 0 ].id
+          }  catch(error) {
+             question_resp_id = 0
+          }
+        } catch(error) {
+          question_resp_id = 0
         }
-      } catch(error) {
-        question_resp_id = 0
-      }
-  
-  if(question_resp_id == 0) {
-    /*** Insert na resposta do formulário ***/
-    const answer = new QuestionResp();
-    answer.fill({
-      application_config_id: body.answer.application_config_id,
-      question_id: body.answer.question_id,
-      phase_id: body.answer.phase_id,
-      interviewer_id: body.answer.interviewer_id, //Não é o User_Id
-      respondent_id: body.answer.respondent_id,
-      answer_yes_no: body.answer.answer_yes_no,
-      answer_comments: body.answer.answer_comments,
-      answer_observation: body.answer.answer_observation
-    })
-    try {
-      await answer.save();
-    } catch(error) {
-      return response
-      .status(400)
-      .send({ message: 'Não foi possível incluir a resposta'})
-    }
+    
+    if(question_resp_id == 0) {
 
-  } else {
-
-    /*** Update na resposta do formulário ***/
-    const answer = await Resposta.findOrFail(question_resp_id);
-    answer.merge({
-      phase_id: body.answer.phase_id,
-      interviewer_id: body.answer.interviewer_id,  //Não é o User_Id              
-      respondent_id: body.answer.respondent_id,
-      answer_yes_no: body.answer.answer_yes_no,
-      answer_comments: body.answer.answer_comments,
-      answer_observation: body.answer.answer_observation
-    })              
-    try {
+      /*** Insert na resposta do formulário ***/
+      const answer = new QuestionResp();
+      answer.fill({
+        application_config_id: body.answer.application_config_id,
+        question_id: body.answer.question_id,
+        phase_id: body.answer.phase_id,
+        interviewer_id: body.answer.interviewer_id, //Não é o User_Id
+        respondent_id: body.answer.respondent_id,
+        answer_yes_no: body.answer.answer_yes_no,
+        answer_comments: body.answer.answer_comments,
+        answer_observation: body.answer.answer_observation
+      })
+      try {
         await answer.save();
       } catch(error) {
         return response
         .status(400)
-        .send({ message: 'Não foi possível atualizar a resposta de Id = '+question_resp_id})
+        .send({ message: 'Não foi possível incluir a resposta'})
       }
-  }
 
-  /* Obtem o número de respostas para esta aplicação do questionário */ 
-  var qtd_respostas = 0
-  try {
-    /* Obtém a quantidade de respostas para questões-mãe esta aplicação */
-    const count_resps = await Database
-    .from('question_resps')
-    .innerJoin('question', 'question_resps.question_id', 'question.id' )
-    .innerJoin('application_configs', 'question_resps.application_config_id', 'application_configs.id' )
-    .where(function () {
-      this
-      .where('question_resps.application_config_id', body.answer.application_config_id)
-      .andWhere('question.question_mother', 'Y')
-      .andWhere(function() {
-        this.where('question_resps.answer_yes_no', '<>', 3)
-      })
-    })
-    .count()
-    try {
-      qtd_respostas = count_resps[0]['count(*)']  // returns number
-    }  catch(error) {
-      qtd_respostas = 0
-    }
-  } catch(error) {
-    qtd_respostas = 0
-  }    
+    } else {
 
-  //console.log('qtd_respostas: '+qtd_respostas) 
-
-  /* Atualizações na tabela application_configs:
-     Status: 1 = Entrevista Em Andamento
-     response_qtd: O número de respostas para esta aplicação
-     question_to_present: A próxima questão a ser apresentada
-     current_session_end_time: Tempo da última resposta
-  */
- try {
-  const affectedRows = await Database  
-  .table('application_configs')
-  .where('id', body.answer.application_config_id)
-  .update({ 
-          status: 1,
-          responses_qtd: qtd_respostas,
-          question_to_present: body.answer.question_id,
-          current_session_end_time: `${now.getHours()}:${now.getMinutes()}`
-          })
-      } catch(error) {
+      /*** Update na resposta do formulário ***/
+      const answer = await Resposta.findOrFail(question_resp_id);
+      answer.merge({
+        phase_id: body.answer.phase_id,
+        interviewer_id: body.answer.interviewer_id,  //Não é o User_Id              
+        respondent_id: body.answer.respondent_id,
+        answer_yes_no: body.answer.answer_yes_no,
+        answer_comments: body.answer.answer_comments,
+        answer_observation: body.answer.answer_observation
+      })              
+      try {
+          await answer.save();
+        } catch(error) {
           return response
           .status(400)
-          .send({ message: 'Não foi possível atualizar a aplicação de id = '+body.answer.application_config_id})
-      }
+          .send({ message: 'Não foi possível atualizar a resposta de Id = '+question_resp_id})
+        }
+    }
 
-    //* Cálculo do current_session_elapsed_time */
-  try {
+    /* Obtem o número de respostas para esta aplicação do questionário */ 
+    var qtd_respostas = 0
+    try {
+      /* Obtém a quantidade de respostas para questões-mãe esta aplicação */
+      const count_resps = await Database
+      .from('question_resps')
+      .innerJoin('question', 'question_resps.question_id', 'question.id' )
+      .innerJoin('application_configs', 'question_resps.application_config_id', 'application_configs.id' )
+      .where(function () {
+        this
+        .where('question_resps.application_config_id', body.answer.application_config_id)
+        .andWhere('question.question_mother', 'Y')
+        .andWhere(function() {
+          this.where('question_resps.answer_yes_no', '<>', 3)
+        })
+      })
+      .count()
+      try {
+        qtd_respostas = count_resps[0]['count(*)']  // returns number
+      }  catch(error) {
+        qtd_respostas = 0
+      }
+    } catch(error) {
+      qtd_respostas = 0
+    }    
+
+    //console.log('qtd_respostas: '+qtd_respostas) 
+
+    /* Atualizações na tabela application_configs:
+       Status: 1 = Entrevista Em Andamento
+       response_qtd: O número de respostas para esta aplicação
+       question_to_present: A próxima questão a ser apresentada
+       current_session_end_time: Tempo da última resposta
+    */
+   try {
+      const affectedRows = await Database  
+      .table('application_configs')
+      .where('id', body.answer.application_config_id)
+      .update({ 
+      status: 1,
+      responses_qtd: qtd_respostas,
+      question_to_present: body.answer.question_id,
+      current_session_end_time: `${now.getHours()}:${now.getMinutes()}`
+      })
+    } catch(error) {
+      return response
+      .status(400)
+      .send({ message: 'Não foi possível atualizar a aplicação de id = '+body.answer.application_config_id})
+    }
+ 
+    /**********************************************************/
+    //*          Cálculo do tempo total de entrevista         */
+    /**********************************************************/   
+    try {
       await Database 
-      .raw('UPDATE application_configs SET current_session_elapsed_time = timediff(current_session_end_time, current_session_start_time) WHERE id = ?', [body.answer.application_config_id])
-      } catch(error) {
+          .raw('UPDATE application_configs SET current_session_elapsed_time = timediff(current_session_end_time, current_session_start_time), interview_total_elapsed_time = ADDTIME(interview_previous_elapsed_time, timediff(current_session_end_time, current_session_start_time)) WHERE id = ?', [body.answer.application_config_id])
+        } catch(error) {
+          return response
+          .status(400)
+          .send({ message: 'Não foi possível calcular o tempo decorrido da aplicação do questionário de id = '+body.answer.application_config_id})
+    }
+ 
+      /**********************************************************/
+      /* Atualiza o registro do Log com o tempo final da sessão */
+      /**********************************************************/
+
+    var log_id = 0;
+    try {
+      /* Obtém o id da sessão mais recente para esta aplicação, 
+      se existir, senão inicia com 0 */    
+      const query_log = await Database.select('id')
+          .from('quest_application_logs')
+          .where(function () {
+            this
+            .where('application_config_id', body.answer.application_config_id)
+            .orderBy ('id','desc')
+          })
+
+      try {
+        log_id = query_log[ 0 ].id
+      }  catch(error) {
+          log_id = 0
+      }
+    } catch(error) {
+      log_id = 0
+    }
+    
+    try {
+      await Database
+      .raw("UPDATE quest_application_logs SET session_end_time = '"+`${now.getHours()}:${now.getMinutes()}`+"' WHERE id = ?", [log_id])
+        } catch(error) {
         return response
         .status(400)
-        .send({ message: 'Não foi possível calcular o tempo decorrido da aplicação do questionário de id = '+body.answer.application_config_id})
-      }
-
-  /* Retorna a próxima questão, incluindo o cálculo da percentagem referente a barra de progresso */
-  return await Questions.query()
-  .innerJoin('questionnaire_versions', 'question.questionnaire_version_id', 'questionnaire_versions.id')
-  .innerJoin('application_configs', 'application_configs.questionnaire_version_id', 'questionnaire_versions.id')
-  .innerJoin('user_parameters', 'user_parameters.application_config_id', 'application_configs.id' )
-  .innerJoin('question_theme', 'question.question_theme_id', 'question_theme.id')
-  .leftOuterJoin('question_resps', 'question.id', 'question_resps.question_id')
-  .where({
-      questionnaire_version_id_carga: body.next.carga,
-      question_edited_number: body.next.question_edited_number 
-    })
-  .andWhere(function() {
-      this.where('application_configs.id', '=', body.answer.application_config_id)
-    })
-  .andWhere(function() {
-      this.where('user_parameters.id', '=', user_id)
-    })      
-  .select(
-    'question.id',
-    'application_configs.id as application_config_id',
-    'user_parameters.id as user_parameter_id',
-    'question.question_theme_id',
-    'question_theme.question_theme',
-    'questionnaire_versions.last_question_number',
-    'application_configs.responses_qtd',
-    'question.phase_id',
-    'question.question_edited_number',
-    'question.if_yes',
-    'question.if_no',
-    'question.if_back',
-    'question.question_enunciation',
-    'question.questionnaire_version_id_carga',
-    'question.questionnaire_version_id',
-    'question_resps.answer_yes_no',
-    'question_resps.answer_comments',
-    'question_resps.answer_observation',
-    'application_configs.current_session_start_time',
-    'application_configs.current_session_end_time',
-    'application_configs.current_session_elapsed_time',
-    'application_configs.interview_total_elapsed_time',
-    'application_configs.status'
-  )
-  .orderBy('question_edited_number')
-  .limit(1)
-  .fetch();    
-}
+        .send({ message: 'Não foi possível atualizar o session_end_time da sessão de id = '+log_id})
+        }    
+  
+    try {
+        await Database
+        .raw('UPDATE quest_application_logs SET session_elapsed_time = timediff(session_end_time, session_start_time) WHERE id = ?', [log_id])
+          } catch(error) {
+          return response
+          .status(400)
+          .send({ message: 'Não foi possível calcular o tempo decorrido da sessão de id = '+log_id})
+          }
+    
+  
+    /* Retorna a próxima questão, incluindo o cálculo da percentagem referente a barra de progresso */
+    return await Questions.query()
+    .innerJoin('questionnaire_versions', 'question.questionnaire_version_id', 'questionnaire_versions.id')
+    .innerJoin('application_configs', 'application_configs.questionnaire_version_id', 'questionnaire_versions.id')
+    .innerJoin('user_parameters', 'user_parameters.application_config_id', 'application_configs.id' )
+    .innerJoin('question_theme', 'question.question_theme_id', 'question_theme.id')
+    .leftOuterJoin('question_resps', 'question.id', 'question_resps.question_id')
+    .where({
+        questionnaire_version_id_carga: body.next.carga,
+        question_edited_number: body.next.question_edited_number 
+      })
+    .andWhere(function() {
+        this.where('application_configs.id', '=', body.answer.application_config_id)
+      })
+    .andWhere(function() {
+        this.where('user_parameters.id', '=', user_id)
+      })      
+    .select(
+      'question.id',
+      'application_configs.id as application_config_id',
+      'user_parameters.id as user_parameter_id',
+      'question.question_theme_id',
+      'question_theme.question_theme',
+      'questionnaire_versions.last_question_number',
+      'application_configs.responses_qtd',
+      'question.phase_id',
+      'question.question_edited_number',
+      'question.if_yes',
+      'question.if_no',
+      'question.if_back',
+      'question.question_enunciation',
+      'question.questionnaire_version_id_carga',
+      'question.questionnaire_version_id',
+      'question_resps.answer_yes_no',
+      'question_resps.answer_comments',
+      'question_resps.answer_observation',
+      'application_configs.current_session_start_time',
+      'application_configs.current_session_end_time',
+      'application_configs.current_session_elapsed_time',
+      'application_configs.interview_total_elapsed_time',
+      'application_configs.status'
+    )
+    .orderBy('question_edited_number')
+    .limit(1)
+    .fetch();    
+  }
 
 
 
