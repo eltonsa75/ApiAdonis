1f58574c37fede1b7dcc588d763241dcca687d06
Elton <eltonsa75@hotmail.com>
2021-03-11 15:44:13 -0300

    Atualizando a Controller Question

--- app/Controllers/Http/QuestionController.js
+++ app/Controllers/Http/QuestionController.js
@@ -126,89 +126,99 @@ class QuestionController {
 */
 async primeiraQuestao ({ request, auth, response}) {
 
-  //Obtém o Id do usuário
-  //const usuarioLogado = await auth.getUser()
-  //const user_id = usuarioLogado.id;
-  //#APIALTERADA#
+    //Obtém o Id do usuário
+    //const usuarioLogado = await auth.getUser()
+    //const user_id = usuarioLogado.id;
+    //#APIALTERADA#
 
- const user_id = 25
+   const user_id = 25
 
- // Leitura dos parâmetros do usuário logado para obter primeira questão
- let user_param
- user_param = await UserParameter.query()
-  .select(
-  'user_parameters.application_config_id',
-  'user_parameters.questionnaire_version_id'
-  )
-  .where('user_parameters.id', '=', user_id)
-  .fetch()
-
-  return await Questions.query()
-  .innerJoin('application_configs', 'application_configs.question_to_present', 'question.id' )
-  .innerJoin('questionnaire_versions', 'questionnaire_versions.id', 'question.questionnaire_version_id' )
-  .innerJoin('user_parameters', 'user_parameters.application_config_id', 'application_configs.id' )
-  .innerJoin('question_theme', 'question_theme.id', 'question.question_theme_id' )    
-  .leftOuterJoin('question_resps', 'question_resps.question_id', 'question.id' )
-  .where('user_parameters.id', user_id)
-  .andWhere('questionnaire_versions.id', user_param.rows[0].questionnaire_version_id)
-  .andWhere('application_configs.id', user_param.rows[0].application_config_id)
-  .select(
-    'question.id',
-    'application_configs.id as application_config_id',
-    'user_parameters.id as user_parameter_id',
-    'question.question_theme_id',
-    'question_theme.question_theme',
-    'questionnaire_versions.last_question_number',
-    'application_configs.responses_qtd',
-    'question.phase_id',
-    'question.question_edited_number',
-    'question.if_yes',
-    'question.if_no',
-    'question.if_back',
-    'question.question_enunciation',
-    'question.questionnaire_version_id_carga',
-    'question.questionnaire_version_id',
-    'question_resps.answer_yes_no',
-    'question_resps.answer_comments',
-    'question_resps.answer_observation',
-    'application_configs.current_session_start_time',
-    'application_configs.current_session_end_time',
-    'application_configs.current_session_elapsed_time',
-    'application_configs.interview_total_elapsed_time'
+   // Leitura dos parâmetros do usuário logado para obter primeira questão
+   let user_param
+   user_param = await UserParameter.query()
+    .select(
+    'user_parameters.application_config_id',
+    'user_parameters.questionnaire_version_id'
     )
-  .orderBy('question_edited_number')
-  .limit(1)
-  .fetch();
-}
+    .where('user_parameters.id', '=', user_id)
+    .fetch()
 
-async proxima ({request}) {
-  return await Questions.query()
-  .leftOuterJoin('question_resps', 'question.id', 'question_resps.question_id')
-  .leftOuterJoin('application_configs', 'question_resps.application_config_id', 'application_configs.id')
-      .where(
-    {
-      questionnaire_version_id_carga: request.params.carga,
-      question_edited_number: request.params.question_edited_number 
-    }
-  )
-  .select(
-    'question.id',
-    'question.question_theme_id',
-    'question.phase_id',
-    'question.question_edited_number',
-    'question.if_yes',
-    'question.if_no',
-    'question.if_back',
-    'question.question_enunciation',
-    'question.questionnaire_version_id_carga',
-    'question.questionnaire_version_id',
-    'question_resps.answer_yes_no',
-    'question_resps.answer_comments',
-    'question_resps.answer_observation'      
-    )
-  .fetch();
+    return await Questions.query()
+    .innerJoin('application_configs', 'application_configs.question_to_present', 'question.id' )
+    .innerJoin('questionnaire_versions', 'questionnaire_versions.id', 'question.questionnaire_version_id' )
+    .innerJoin('user_parameters', 'user_parameters.application_config_id', 'application_configs.id' )
+    .innerJoin('question_theme', 'question_theme.id', 'question.question_theme_id' )    
+    .leftOuterJoin('question_resps', 'question_resps.question_id', 'question.id' )
+    .where('user_parameters.id', user_id)
+    .andWhere('questionnaire_versions.id', user_param.rows[0].questionnaire_version_id)
+    .andWhere('application_configs.id', user_param.rows[0].application_config_id)
+    .select(
+      'question.id',
+      'application_configs.id as application_config_id',
+      'user_parameters.id as user_parameter_id',
+      'question.question_theme_id',
+      'question_theme.question_theme',
+      'questionnaire_versions.last_question_number',
+      'application_configs.responses_qtd',
+      'question.phase_id',
+      'question.question_edited_number',
+      'question.if_yes',
+      'question.if_no',
+      'question.if_back',
+      'question.question_enunciation',
+      'question.questionnaire_version_id_carga',
+      'question.questionnaire_version_id',
+      'question_resps.answer_yes_no',
+      'question_resps.answer_comments',
+      'question_resps.answer_observation',
+      'application_configs.current_session_start_time',
+      'application_configs.current_session_end_time',
+      'application_configs.current_session_elapsed_time',
+      'application_configs.interview_total_elapsed_time'
+      )
+    .orderBy('question_edited_number')
+    .limit(1)
+    .fetch();
   }
 
+  async proxima ({request}) {  
+    return await Questions.query()
+    .leftOuterJoin('questionnaire_versions', 'question.questionnaire_version_id', 'questionnaire_versions.id')
+    .leftOuterJoin('question_theme', 'question.question_theme_id', 'question_theme.id')
+    .leftOuterJoin('question_resps', 'question.id', 'question_resps.question_id')
+    .leftOuterJoin('application_configs', 'question_resps.application_config_id', 'application_configs.id')
+    .where(
+        {
+          application_config_id: request.params.carga,
+          question_edited_number: request.params.question_edited_number
+          /*
+          questionnaire_version_id_carga: body.next.carga,
+          question_edited_number: body.next.question_edited_number 
+          */
+        }
+      )
+      .andWhere('question_resps.application_config_id', application_config_id)
+      .select
+      (
+        'question.id',
+        'question.question_theme_id',
+        'question_theme.question_theme',
+        'questionnaire_versions.last_question_number',
+        'application_configs.responses_qtd',
+        'question.phase_id',
+        'question.question_edited_number',
+        'question.if_yes',
+        'question.if_no',
+        'question.if_back',
+        'question.question_enunciation',
+        'question.questionnaire_version_id_carga',
+        'question.questionnaire_version_id',
+        'question_resps.answer_yes_no',
+        'question_resps.answer_comments',
+        'question_resps.answer_observation'
+      )
+      .fetch();
+  }
 
 
   async save_and_next_original ({request, auth}) {
@@ -292,6 +302,7 @@ async proxima ({request}) {
  /*************** */
   /* Save_And_Next */
   /*************** */
+  
   async save_and_next ({request}) {
     
     const body = JSON.parse(request.body.json);
@@ -361,6 +372,12 @@ async proxima ({request}) {
           .status(400)
           .send({ message: 'Não foi possível atualizar a resposta de Id = '+question_resp_id})
         }
+
+      /*** Exclusão das respostas que ficaram soltas (filhas)                             ***/
+      /*** Se resposta atual for Não, verificar conteudo do campo "question_mother_if_no" ***
+       *** Se o campo for preenchido com a questão mãe, as filhas devem ser excluídas     ***/
+      /*** Exemplo: Se question_mother_if_no = "01", devem ser excluidas as respostas das ***/
+      /***          questões 01.1, 01.2, 01.2.1, etc - utilizar na pesquisa "01.%"        ***/
     }
 
     /* Obtem o número de respostas para esta aplicação do questionário */ 
@@ -515,11 +532,6 @@ async proxima ({request}) {
     .limit(1)
     .fetch();    
   }
-
-
-
-
-
 
 }
 
